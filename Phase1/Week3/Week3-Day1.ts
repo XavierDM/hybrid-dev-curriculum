862d866a9c3df1d0feacb6649492c3094decf8b8
XavierDM <xavier.de.moor@gmail.com>
2026-03-19 15:26:11 +0100

    started creating custom DTO

--- Phase1/Week3/Week3-Day1.ts
+++ Phase1/Week3/Week3-Day1.ts
@@ -6,9 +6,10 @@ interface User {
   role: 'admin' | 'user';
 }
 
-
-type UserUpdate = Partial<User> //all optional
+type UserUpdate = Partial<User>; //all optional
 type PublicUser = Pick<User, 'id' | 'name' | 'role'>; //Select fields to pick and send to client
-type UserWithoutPassword = Omit <User, 'password'> //exclude fields
-type ImmutableUser = Readonly<User> //nothing can change
-type UserMap = Record<string, User> 
+type UserWithoutPassword = Omit<User, 'password'>; //exclude fields
+type ImmutableUser = Readonly<User>; //nothing can change
+type UserMap = Record<string, User>;
+
+//type CreateUserDto = Omit<User, 'name', 'email'>;
